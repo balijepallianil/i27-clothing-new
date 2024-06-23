d25fbcdc650134d4d4e42b4a452fd623875248f4
balijepallianil <balijepallianilg@gmail.com>
2024-06-23 10:08:18 +0530

    1073

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const produtsServiceLocalHost = 'http://localhost:8132/api'
 const usersServiceLocalHost = 'http://localhost:8232/api'
 
-const produtsServiceDevHost = 'http://35.196.148.247:5132/api'
-const usersServiceDevHost = 'http://35.196.148.247:5232/api'
+const produtsServiceDevHost = 'http://35.247.48.119:31987/api'
+const usersServiceDevHost = 'http://35.247.48.119:32737/api'
 
 const produtsServiceTestHost = ''
 const usersServiceTestHost = ''
